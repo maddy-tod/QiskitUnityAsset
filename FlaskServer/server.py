972ab93a89297a54eca346f01730920fb4971a6b
madeleine-tod <madeleine.tod@ibm.com>
2019-06-25 16:21:58 +0100

    updated backend to have terra player

--- FlaskServer/server.py
+++ FlaskServer/server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-from flask import request
+from flask import request, Flask
 from flask import jsonify
 from api import run_qasm
+from tictactoe import terra_move
 import json
 
 app = Flask(__name__)
@@ -20,6 +21,22 @@ def qasm():
     backend = 'qasm_simulator'
     output = run_qasm(qasm, backend)
     ret = {"result": output}
+    return jsonify(ret)
+
+@app.route('/tictactoe/player/terra', methods=['POST'])
+def terra():
+    board = request.form.get('board')
+    print("--------------")
+    print (board)
+    print(request.get_data())
+    print (request.form)
+
+    board = board.split(',')
+
+    (move, t_counts) = terra_move(board)
+    ret = {"move": move, "t_counts": t_counts}
+
+    print('sending : ', ret)
     return jsonify(ret)
 
 if __name__ == '__main__':
---
+++ FlaskServer/tictactoe.py
@@ -0,0 +1,166 @@
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
+
+
+def terra_move(board):
+    """
+    Args:
+        board [int] : the current state of the board
+    Returns:
+        (int, dict) : (the index of the move to be made, the t counts)
+    """
+
+    num_qubits = 9
+    # reset the T gate counter
+    num_t_gates = [0] * num_qubits
+    q = QuantumRegister(num_qubits)
+    c = ClassicalRegister(num_qubits)
+    qc = QuantumCircuit(q, c)
+
+    # None comes through as -1
+    board = [None if int(space) == -1 else int(space) for space in board]
+
+    # make the option definitely a 1 if there is a move in the space already
+    for index, move in enumerate(board):
+
+        if move:
+            qc.x(q[index])
+        else:
+            # this space is a potential move
+            # so put into a superposition
+            qc.h(q[index])
+
+            t_count = 0
+
+            # so it is the start of a row
+            if index % 3 == 0:
+
+                # two pieces in a row - need to block/win
+                if board[index + 1] and board[index + 2] and board[index + 1] == board[index + 2]:
+                    qc.t(q[index])
+                    qc.t(q[index])
+                    t_count += 2
+                # only one of the spaces is occupied (^ is xor, but they both have to explicitly be bools)
+                elif bool(board[index + 1]) ^ bool(board[index + 2]):
+                    qc.t(q[index])
+                    t_count += 1
+
+            # so it is the middle of a row
+            if index % 3 == 1:
+
+                # two pieces in a row - need to block/win
+                if board[index + 1] and board[index - 1] and board[index + 1] == board[index - 1]:
+                    qc.t(q[index])
+                    qc.t(q[index])
+                    t_count += 2
+                elif bool(board[index + 1]) ^ bool(board[index - 1]):
+                    qc.t(q[index])
+                    t_count += 1
+
+            # so it is the end of a row
+            if index % 3 == 2:
+                # two pieces in a row - need to block/win
+                if board[index - 1] and board[index - 2] and board[index - 1] == board[index - 2]:
+                    qc.t(q[index])
+                    qc.t(q[index])
+                    t_count += 2
+                elif bool(board[index - 1]) ^ bool(board[index - 2]):
+                    qc.t(q[index])
+                    t_count += 1
+
+            # so is the top row
+            if index / 3 < 1:
+                if board[index + 3] and board[index + 6] and board[index + 3] == board[index + 6]:
+                    qc.t(q[index])
+                    qc.t(q[index])
+                    t_count += 2
+                elif bool(board[index + 3]) ^ bool(board[index + 6]):
+                    qc.t(q[index])
+                    t_count += 1
+
+            # so it is the middle row
+            if 2 > index / 3 >= 1:
+                if board[index - 3] and board[index + 3] and board[index - 3] == board[index + 3]:
+                    qc.t(q[index])
+                    qc.t(q[index])
+                    t_count += 2
+                elif bool(board[index - 3]) ^ bool(board[index + 3]):
+                    qc.t(q[index])
+                    t_count += 1
+
+            # so it is the top row
+            if index / 3 >= 2:
+                if board[index - 3] and board[index - 6] and board[index - 3] == board[index - 6]:
+                    qc.t(q[index])
+                    qc.t(q[index])
+                    t_count += 2
+                elif bool(board[index - 3]) ^ bool(board[index - 6]):
+                    qc.t(q[index])
+                    t_count += 1
+
+            num_t_gates[index] = t_count
+
+    # hard code in the diagonals
+    if board[0] and board[0] == board[4]:
+        qc.t(q[8])
+        qc.t(q[8])
+        qc.t(q[8])
+        num_t_gates[8] += 3
+    if board[0] and board[0] == board[8]:
+        qc.t(q[4])
+        qc.t(q[4])
+        qc.t(q[4])
+        num_t_gates[4] += 3
+    if board[4] and board[4] == board[8]:
+        qc.t(q[0])
+        qc.t(q[0])
+        qc.t(q[0])
+        num_t_gates[0] += 3
+    if board[2] and board[2] == board[4]:
+        qc.t(q[6])
+        qc.t(q[6])
+        qc.t(q[6])
+        num_t_gates[6] += 3
+    if board[2] and board[2] == board[6]:
+        qc.t(q[4])
+        qc.t(q[4])
+        qc.t(q[4])
+        num_t_gates[4] += 3
+    if board[4] and board[4] == board[6]:
+        qc.t(q[2])
+        qc.t(q[2])
+        qc.t(q[2])
+        num_t_gates[2] += 3
+
+    for index, move in enumerate(board):
+        if not move:
+            qc.h(q[index])
+        else:
+            # if there is already a move there - don't show that any t gates were applied
+            num_t_gates[index] = -1
+    qc.measure(q, c)
+
+    print("Made the circuit, running it on the backend")
+    backend = Aer.get_backend('qasm_simulator')
+    shots = 100
+    job_sim = execute(qc, backend, shots=shots)
+    sim_result = job_sim.result().get_counts(qc)
+
+    counts = [0]*num_qubits
+
+    for key, count in sim_result.items():
+        # need to iterate over the results and see when the value was chosen most
+
+        # keys are the opposite way round to expected
+        key = key[::-1]
+        for index, val in enumerate(key):
+            if val == '1':
+                counts[index] += count
+
+    max_count = 0
+    max_index = 0
+    for index, count in enumerate(counts):
+        if not board[index] and count > max_count:
+            max_index = index
+            max_count = count
+
+    return (max_index, num_t_gates)
